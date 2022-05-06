159d2de0ccefb16dd4836586cd08f252efcd8fd5
svhenrique <silviohenrique1234@hotmail.com>
2022-05-06 15:16:01 -0300

    fix: WatchableContent and Episode relationship #15 - Removed episode ForeignKey of WatchableContent; - Added content ForeignKey (of WatchableContent) in episode - Fixed fields in admin based on models updates; - Fixed serializers based on models updates;

--- whattowatch/core/serializers.py
+++ whattowatch/core/serializers.py
@@ -4,12 +4,12 @@ from core.models import Episode, WatchableContent, HighlightedArea
 class EpisodeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Episode
-        fields = ('name', 'episode', 'description', 'season', 'video')
+        fields = ('name', 'episode', 'content', 'description', 'season', 'video')
 
 class WatchableContentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = WatchableContent
-        fields = ('title', 'episodes', 'description', 'ratting_age', 'poster',
+        fields = ('title', 'description', 'ratting_age', 'poster',
         'genres', 'daily_views', 'week_views', 'year_views')
 
 class HighlightedAreaSerializer(serializers.HyperlinkedModelSerializer):
