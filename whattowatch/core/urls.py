b246991a2e0cea067d5d405dc655ac63843b92b6
svhenrique <silviohenrique1234@hotmail.com>
2022-05-06 15:24:59 -0300

    feat: added autentication for post, put, patch and delete #14 - Created read, list, create and detail view classes for each model; - Added changes of view classes in urls;

--- whattowatch/core/urls.py
+++ whattowatch/core/urls.py
@@ -2,12 +2,18 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('episode/', views.EpisodeList.as_view(), name='episode-list'),    
-    path('episode/<int:pk>', views.EpisodeDetail.as_view(), name='episode-detail'),
+    path('episodes/', views.EpisodeList.as_view(), name='episode-list'),
+    path('episode/create/', views.EpisodeCreate.as_view(), name='episode-post'),      
+    path('episode/read/<int:pk>', views.EpisodeRead.as_view(), name='episode-get'),   
+    path('episode/detail/<int:pk>', views.EpisodeDetail.as_view(), name='episode-detail'),
 
-    path('content/', views.WatchableContentList.as_view(), name='content-list'),    
-    path('content/<int:pk>', views.WatchableContentDetail.as_view(), name='content-detail'),
+    path('contents/', views.WatchableContentList.as_view(), name='watchablecontent-list'),    
+    path('content/create/', views.WatchableContentCreate.as_view(), name='watchablecontent-post'),        
+    path('content/read/<int:pk>', views.WatchableContentRead.as_view(), name='watchablecontent-get'),
+    path('content/detail/<int:pk>', views.WatchableContentDetail.as_view(), name='watchablecontent-detail'),
 
-    path('higharea/', views.HighlightedAreaList.as_view(), name='higharea-list'),    
-    path('higharea/<int:pk>', views.HighlightedAreaDetail.as_view(), name='higharea-detail'),
+    path('highareas/', views.HighlightedAreaList.as_view(), name='HighlightedAreaList-list'),    
+    path('higharea/create/', views.HighlightedAreaCreate.as_view(), name='HighlightedAreaList-post'),     
+    path('higharea/read/<int:pk>', views.HighlightedAreaRead.as_view(), name='HighlightedAreaList-get'),   
+    path('higharea/detail/<int:pk>', views.HighlightedAreaDetail.as_view(), name='HighlightedAreaList-detail'),
 ]
