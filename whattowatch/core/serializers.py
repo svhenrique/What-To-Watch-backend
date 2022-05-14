a3061a6dc23b414db9602459ebd2701c6a58039f
svhenrique <silviohenrique1234@hotmail.com>
2022-05-14 00:57:26 -0300

    feat: added release_date datafield #19 - Added release_date field in WatchableContent - Added release_date field in WhatchableContentSerializer

--- whattowatch/core/serializers.py
+++ whattowatch/core/serializers.py
@@ -10,7 +10,7 @@ class WatchableContentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = WatchableContent
         fields = ('title', 'description', 'ratting_age', 'poster',
-        'genres', 'daily_views', 'week_views', 'year_views')
+        'genres', 'release_date', 'daily_views', 'week_views', 'year_views')
 
 class HighlightedAreaSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
